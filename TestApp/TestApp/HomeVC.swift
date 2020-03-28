15a5edd59aebd109837a931ffc333d909f89756d
Anilkumar18 <anilkumarunni@gmail.com>
2020-03-28 15:57:52 +0530

    test commit3

--- TestApp/TestApp/HomeVC.swift
+++ TestApp/TestApp/HomeVC.swift
@@ -14,7 +14,7 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "HockeyApp Test"
-        //testtest
+        //testtesttets
         // Do any additional setup after loading the view.
     }
 
