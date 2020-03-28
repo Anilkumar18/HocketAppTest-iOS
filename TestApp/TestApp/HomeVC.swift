cbfca40f525874054803d366b36a9f2f56c65e13
Anilkumar18 <anilkumarunni@gmail.com>
2020-03-28 17:00:55 +0530

    added version alert

--- TestApp/TestApp/HomeVC.swift
+++ TestApp/TestApp/HomeVC.swift
@@ -14,13 +14,29 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "HockeyApp Test"
-        //testtesttetstest
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func actionGenerateCrash(_ sender: Any) {
         MSAnalytics.trackEvent("Generating Crash")
         let array: [String] = []
         print(array[0])
+    }
+    @IBAction func actionGetBuildVersion(_ sender: Any) {
+        let versionAlert = UIAlertController(title: "Version Alert", message: "The Current Version is \(String(describing: Bundle.main.buildVersionNumber))", preferredStyle: .alert)
+        versionAlert.addAction(UIAlertAction(title: "Accept", style: .default) { (action:UIAlertAction!) in
+                print("Selected")
+            })
+            self.present(versionAlert, animated: true)
+    }
+}
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var releaseVersionNumberPretty: String {
+        return "v\(releaseVersionNumber ?? "1.0.0")"
     }
 }
